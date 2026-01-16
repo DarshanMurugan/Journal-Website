829c88e2db71eb83fee52d3e8d6ccb77a90c1d7a
DarshanMurugan <darshanmurugan56@gmail.com>
2026-01-16 21:53:37 +0530

    Initial commit: Journal Website

--- back-end/journal_back_end/settings.py
+++ back-end/journal_back_end/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@40%$!uc3%o1!e#0l52xkx%(bp%pis0d^$k-79qidi)&h2z#4+'
+env_path = BASE_DIR.parent / '.env'
+load_dotenv(dotenv_path=env_path)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
